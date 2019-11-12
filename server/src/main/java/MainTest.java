ead2c9e8bf2c03e166f14d8f5ef9ffcb87b017db
JADGardner <iamjamesgardner@googlemail.com>
2019-11-12 12:41:32 +0000

    Updated project to Maven project.
    Created a multi-module maven project. Using Maven to handle dependancies.

---
+++ server/src/main/java/MainTest.java
@@ -0,0 +1,20 @@
+/*Created along with the MainTest class in the Client module just to
+ * test both modules building and running simultaneously. */
+
+import javax.swing.*;
+
+public class MainTest {
+
+    public MainTest(){
+        JFrame frame = new JFrame("Test Server");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+    }
+    public static void main(String[] args) {
+        MainTest main = new MainTest();
+    }
+}
