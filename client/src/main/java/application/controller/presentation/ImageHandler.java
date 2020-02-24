bbee17ea0af6c3a10c16ecbf2529eef1157b2a78
Daniel Bishop <dmb537@its.york.ac.uk>
2020-02-24 13:21:49 +0000

    Some StyleGuide Fixes

--- client/src/main/java/application/controller/presentation/ImageHandler.java
+++ client/src/main/java/application/controller/presentation/ImageHandler.java
@@ -2,52 +2,56 @@ package application.controller.presentation;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 
 /**
- * CLASS DESCRIPTION:
+ * Module to draw image files onto a StackPlane by adding extra canvas.
+ * Also handles deleting images via an ID assigned when drawn.
  *
  * @author Daniel Bishop
  *
  */
 public class ImageHandler {
 
-    private StackPane pane;
-    private Map<String, Canvas> images = new HashMap<>();
+  private StackPane pane;
+  private Map<String, Canvas> images = new HashMap<>();
 
-    public ImageHandler(StackPane targetPane) {
-        this.pane = targetPane;
-       
-    }
+  public ImageHandler(StackPane targetPane) {
+    this.pane = targetPane;
+  }
 
-    public String drawImage(String url, String id, int x, int y, int w, int h){
+  /**
+   * Draw an image from a URL onto the StackPlane, using the provided ID, dimensions and location.
+  */
+  public String drawImage(String url, String id, int x, int y, int w, int h) {
         
-        Image picture = new Image(url, w, h, false, true);
-        Canvas newCanvas = new Canvas((double) w + x, (double) h + y);
+    Image picture = new Image(url, w, h, false, true);
+    Canvas newCanvas = new Canvas((double) w + x, (double) h + y);
 
-        GraphicsContext gc = newCanvas.getGraphicsContext2D();
-        gc.drawImage(picture, x, y);
+    GraphicsContext gc = newCanvas.getGraphicsContext2D();
+    gc.drawImage(picture, x, y);
 
-        if(images.putIfAbsent(id, newCanvas) == null){
-            pane.getChildren().add(newCanvas);
-            return id;
-        } else {
-            return null;
-        }
+    if (images.putIfAbsent(id, newCanvas) == null) {
+      pane.getChildren().add(newCanvas);
+      return id;
+    } else {
+      return null;
     }
+  }
 
-    public boolean remove(String id){
-        if (images.containsKey(id)){
-            pane.getChildren().remove(images.get(id));
-            images.remove(id);
-            return true;
-        } else {
-            return false;
-        }
-        
+  /**
+   * Remove the image with the provided ID.
+  */
+  public boolean remove(String id) {
+    if (images.containsKey(id)) {
+      pane.getChildren().remove(images.get(id));
+      images.remove(id);
+      return true;
+    } else {
+      return false;
     }
+  }
 }
--- client/src/main/java/application/controller/presentation/PresentationSlide.java
+++ client/src/main/java/application/controller/presentation/PresentationSlide.java
@@ -4,60 +4,71 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /**
- * CLASS DESCRIPTION:
+ * CLASS DESCRIPTION.
  *
  * @author CUBIXEL
  *
  */
 public class PresentationSlide {
-    private int id;
-    public PresentationSlide(Node slide){
-        NodeList children = slide.getChildNodes();
-        for (int i = 0; i < children.getLength(); i++){
-            Node childNode = children.item(i);
-            String nodeName = childNode.getNodeName();
-            switch (nodeName) {
-                case "id":
-                    id = Integer.parseInt(childNode.getNodeValue());
-                    break;
-                case "text":
-                    addText(childNode);
-                    break;
-                case "line":
-                    addLine(childNode);
-                    break;
-                case "shape":
-                    addShape(childNode);
-                    break;
-                case "audio":
-                    addAudio(childNode);
-                    break;
-                case "image":
-                    addImage(childNode);
-                    break;
-                case "video":
-                    addVideo(childNode);
-                    break;
+  private int id;
+
+  /**
+   * CONSTRUCTOR DESCRIPTION.
+   */
+  public PresentationSlide(Node slide) {
+    NodeList children = slide.getChildNodes();
+    for (int i = 0; i < children.getLength(); i++) {
+      Node childNode = children.item(i);
+      String nodeName = childNode.getNodeName();
+      switch (nodeName) {
+        case "id":
+          id = Integer.parseInt(childNode.getNodeValue());
+          break;
+        case "text":
+          addText(childNode);
+          break;
+        case "line":
+          addLine(childNode);
+          break;
+        case "shape":
+          addShape(childNode);
+          break;
+        case "audio":
+          addAudio(childNode);
+          break;
+        case "image":
+          addImage(childNode);
+          break;
+        case "video":
+          addVideo(childNode);
+          break;
             
-                default:
-                    break;
-            }
-        }
+        default:
+          break;
+      }
     }
-	private void addVideo(Node childNode) {
-	}
-	private void addImage(Node childNode) {
-	}
-	private void addAudio(Node childNode) {
-	}
-	private void addShape(Node childNode) {
-	}
-	private void addLine(Node childNode) {
-	}
-	private void addText(Node childNode) {
-    }
-	public Object getId() {
-		return null;
-	}
+  }
+
+  private void addVideo(Node childNode) {
+  }
+    
+  private void addImage(Node childNode) {
+  }
+    
+  private void addAudio(Node childNode) {
+  }
+    
+  private void addShape(Node childNode) {
+  }
+    
+  private void addLine(Node childNode) {
+  }
+    
+  private void addText(Node childNode) {
+  }
+    
+  public Object getId() {
+    return id;
+  }
 
 }
