9e1afaa0c09a4dcc356a880e641bdd8856945e01
Daniel Bishop <dmb537@its.york.ac.uk>
2020-02-24 14:16:41 +0000

    Controller Tests

--- client/src/test/java/application/controller/LoginWindowControllerTest.java
+++ client/src/test/java/application/controller/LoginWindowControllerTest.java
@@ -11,8 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
-import org.mockito.Mock;
-
 import application.controller.services.LoginService;
 import application.controller.services.MainConnection;
 import application.view.ViewFactory;
@@ -20,6 +18,7 @@ import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.mockito.Mock;
 
 /**
  * CLASS DESCRIPTION:
@@ -32,46 +31,50 @@ import javafx.scene.control.TextField;
  */
 public class LoginWindowControllerTest {
 
-    /* Creating the Mock Objects necessary for the test. */
-    @Mock
-    protected MainConnection mainConnectionMock;
+  /* Creating the Mock Objects necessary for the test. */
+  @Mock
+  protected MainConnection mainConnectionMock;
 
-    @Mock
-    protected ViewFactory viewFactoryMock;
+  @Mock
+  protected ViewFactory viewFactoryMock;
 
-    @Mock
-    protected LoginService loginServiceMock;
+  @Mock
+  protected LoginService loginServiceMock;
 
-    /* Creating local JavaFX Objects for testing. */
-    protected TextField usernameField;
+  /* Creating local JavaFX Objects for testing. */
+  protected TextField usernameField;
 
-    protected PasswordField passwordField;
+  protected PasswordField passwordField;
 
-    protected Label errorLabel;
+  protected Label errorLabel;
 
-    protected LoginWindowController loginWindowController;
+  protected LoginWindowController loginWindowController;
 
-    /* This is testing pressing the Login Button before entering a
-     * String into the username and password fields. */
-    public void testFieldsValidation(){
-        loginWindowController.loginButtonAction();
-        assertEquals(errorLabel.getText(), "Please Enter Username");
-        usernameField.setText("someUsername");
-        loginWindowController.loginButtonAction();
-        assertEquals(errorLabel.getText(), "Please Enter Password");
-        System.out.println("Tested Login Fields Validation");
-    }
+  /**
+   *  This is testing pressing the Login Button before entering a
+   * String into the username and password fields.
+   */
+  public void testFieldsValidation() {
+    loginWindowController.loginButtonAction();
+    assertEquals(errorLabel.getText(), "Please Enter Username");
+    usernameField.setText("someUsername");
+    loginWindowController.loginButtonAction();
+    assertEquals(errorLabel.getText(), "Please Enter Password");
+    System.out.println("Tested Login Fields Validation");
+  }
 
-    /* This is testing that the loginService is started correctly once
-     * Strings are in both fields and the user presses the Login Button. */
-    public void testLoginAction(){
-        Platform.runLater(() ->{
-            usernameField.setText("someUsername");
-            passwordField.setText("password");
-            loginWindowController.loginButtonAction();
-            verify(loginServiceMock).setAccount(any());
-            verify(loginServiceMock).start();
-            System.out.println("Tested Login Action");
-        });
-    }
+  /**
+   * This is testing that the loginService is started correctly once
+   * Strings are in both fields and the user presses the Login Button.
+   */
+  public void testLoginAction() {
+    Platform.runLater(() -> {
+      usernameField.setText("someUsername");
+      passwordField.setText("password");
+      loginWindowController.loginButtonAction();
+      verify(loginServiceMock).setAccount(any());
+      verify(loginServiceMock).start();
+      System.out.println("Tested Login Action");
+    });
+  }
 }
--- client/src/test/java/application/controller/RegisterWindowControllerTest.java
+++ client/src/test/java/application/controller/RegisterWindowControllerTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
-import org.mockito.Mock;
-
 import application.controller.services.MainConnection;
 import application.controller.services.RegisterService;
 import application.view.ViewFactory;
@@ -14,51 +12,56 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.mockito.Mock;
 
 public class RegisterWindowControllerTest {
 
-    /* Creating the Mock Objects necessary for the test. */
-    @Mock
-    protected MainConnection mainConnectionMock;
+  /* Creating the Mock Objects necessary for the test. */
+  @Mock
+  protected MainConnection mainConnectionMock;
 
-    @Mock
-    protected ViewFactory viewFactoryMock;
+  @Mock
+  protected ViewFactory viewFactoryMock;
 
-    @Mock
-    protected RegisterService registerServiceMock;
+  @Mock
+  protected RegisterService registerServiceMock;
 
-    /* Creating local JavaFX Objects for testing. */
-    protected TextField usernameField;
+  /* Creating local JavaFX Objects for testing. */
+  protected TextField usernameField;
 
-    protected PasswordField passwordField;
+  protected PasswordField passwordField;
 
-    protected Label errorLabel;
+  protected Label errorLabel;
 
-    protected CheckBox isTutorCheckBox;
+  protected CheckBox isTutorCheckBox;
 
-    protected RegisterWindowController registerWindowController;
+  protected RegisterWindowController registerWindowController;
 
-    /* This is testing pressing the Login Button before entering a
-     * String into the username and password fields. */
-    public void testFieldsValidation(){
-        registerWindowController.registerButtonAction();
-        assertEquals(errorLabel.getText(), "Please Enter Username");
-        usernameField.setText("someUsername");
-        registerWindowController.registerButtonAction();
-        assertEquals(errorLabel.getText(), "Please Enter Password");
-        System.out.println("Tested Register Fields Action");
-    }
+  /**
+   * This is testing pressing the Login Button before entering a
+   * String into the username and password fields.
+   */
+  public void testFieldsValidation() {
+    registerWindowController.registerButtonAction();
+    assertEquals(errorLabel.getText(), "Please Enter Username");
+    usernameField.setText("someUsername");
+    registerWindowController.registerButtonAction();
+    assertEquals(errorLabel.getText(), "Please Enter Password");
+    System.out.println("Tested Register Fields Action");
+  }
 
-    /* This is testing that the loginService is started correctly once
-     * Strings are in both fields and the user presses the Login Button. */
-    public void testRegisterAction(){
-        Platform.runLater(() -> {
-            usernameField.setText("someUsername");
-            passwordField.setText("password");
-            registerWindowController.registerButtonAction();
-            verify(registerServiceMock).setAccount(any());
-            verify(registerServiceMock).start();
-            System.out.println("Tested Register Action");
-        });
-    }
+  /**
+   * This is testing that the loginService is started correctly once
+   * Strings are in both fields and the user presses the Login Button.
+   */
+  public void testRegisterAction() {
+    Platform.runLater(() -> {
+      usernameField.setText("someUsername");
+      passwordField.setText("password");
+      registerWindowController.registerButtonAction();
+      verify(registerServiceMock).setAccount(any());
+      verify(registerServiceMock).start();
+      System.out.println("Tested Register Action");
+    });
+  }
 }
