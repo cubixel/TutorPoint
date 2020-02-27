a3a4aa3633f4b0e4f4129353645a2ae976cefa74
JADGardner <iamjamesgardner@googlemail.com>
2020-02-27 13:25:12 +0000

    Named HBox Field Created some thumbnails

--- client/src/main/java/application/controller/services/SubjectRendererService.java
+++ client/src/main/java/application/controller/services/SubjectRendererService.java
@@ -9,38 +9,36 @@ import java.io.IOException;
 
 public class SubjectRendererService extends Service<Void> {
 
-    private MainConnection connection;
-    private SubjectManager subjectManager;
+  private MainConnection connection;
+  private SubjectManager subjectManager;
 
-    public SubjectRendererService(MainConnection connection){
-        this.connection = connection;
-        subjectManager = new SubjectManager();
+  public SubjectRendererService(MainConnection connection) {
+    this.connection = connection;
+    subjectManager = new SubjectManager();
+  }
+
+  private void fetchSubjects() {
+    try {
+      connection.sendString(connection.packageClass(this.subjectManager));
+      String serverReply = connection.listenForString();
+      //return new Gson().fromJson(serverReply, AccountRegisterResult.class);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    private void fetchSubjects(){
+  @Override
+  protected Task<Void> createTask() {
+    return new Task<>() {
+      @Override
+      protected Void call() throws Exception {
         try {
-            connection.sendString(connection.packageClass(this.subjectManager));
-            String serverReply = connection.listenForString();
-            //return new Gson().fromJson(serverReply, AccountRegisterResult.class);
-        } catch (IOException e){
-            e.printStackTrace();
-        } catch (Exception e){
-            e.printStackTrace();
+          fetchSubjects();
+        } catch (Exception e) {
+          e.printStackTrace();
         }
-    }
-
-    @Override
-    protected Task<Void> createTask() {
-        return new Task<Void>() {
-            @Override
-            protected Void call() throws Exception {
-                try {
-                    fetchSubjects();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-        };
-    }
+        return null;
+      }
+    };
+  }
 }
--- server/src/main/java/ClientHandler.java
+++ server/src/main/java/ClientHandler.java
@@ -14,139 +14,175 @@ import sql.MySQL;
 
 public class ClientHandler extends Thread {
 
-    private int token;
-    private final DataInputStream dis;
-    private final DataOutputStream dos;
-    private MySQL sqlConnection;
-    private long lastHeartbeat;
-    private boolean loggedIn;
+  private int token;
+  private final DataInputStream dis;
+  private final DataOutputStream dos;
+  private MySQL sqlConnection;
+  private long lastHeartbeat;
+  private boolean loggedIn;
 
-    // int numberOfSubjectSent
+  // int numberOfSubjectSent
 
-    public ClientHandler(DataInputStream dis, DataOutputStream dos, int token, MySQL sqlConnection){
-        setDaemon(true);
-        this.dis = dis;
-        this.dos = dos;
-        this.token = token;
-        this.sqlConnection = sqlConnection;
-        this.lastHeartbeat = System.currentTimeMillis();
-        this.loggedIn = true;
-    }
+  /**
+   * CLASS DESCRIPTION.
+   * #################
+   *
+   * @author CUBIXEL
+   *
+   */
+  public ClientHandler(DataInputStream dis, DataOutputStream dos, int token, MySQL sqlConnection){
+      setDaemon(true);
+      this.dis = dis;
+      this.dos = dos;
+      this.token = token;
+      this.sqlConnection = sqlConnection;
+      this.lastHeartbeat = System.currentTimeMillis();
+      this.loggedIn = true;
+  }
 
-    @Override
-    public void run() {
-        // Does the client need to know its number?
-        //writeString("Token#" + token);
-        String received = null;
+  /**
+   * CLASS DESCRIPTION.
+   * #################
+   *
+   * @author CUBIXEL
+   *
+   */
+  @Override
+  public void run() {
+    // Does the client need to know its number?
+    //writeString("Token#" + token);
+    String received = null;
 
-        while (lastHeartbeat > (System.currentTimeMillis() - 10000) & loggedIn){
-            // Do stuff with this client in this thread
-            // when client disconnects then close it down.
+    while (lastHeartbeat > (System.currentTimeMillis() - 10000) & loggedIn) {
+      // Do stuff with this client in this thread
+      // when client disconnects then close it down.
 
-            try {
+      try {
 
-                while(dis.available() > 0) {
-                    received = dis.readUTF();
-                }
+        while (dis.available() > 0) {
+          received = dis.readUTF();
+        }
 
-                if (received != null){
-                    try {
-                        Gson gson = new Gson();
-                        JsonObject jsonObject = gson.fromJson(received, JsonObject.class);
-                        String action = jsonObject.get("Class").getAsString();
-                        System.out.println(action);
+        if (received != null) {
+          try {
+            Gson gson = new Gson();
+            JsonObject jsonObject = gson.fromJson(received, JsonObject.class);
+            String action = jsonObject.get("Class").getAsString();
+            System.out.println(action);
 
-                        if (action.equals("Account")) {
-                            if (jsonObject.get("isRegister").getAsInt() == 1) {
-                                createNewUser(jsonObject.get("username").getAsString(), jsonObject.get("hashedpw").getAsString(),
-                                        jsonObject.get("tutorStatus").getAsInt());
-                            }else {
-                                loginUser(jsonObject.get("username").getAsString(), jsonObject.get("hashedpw").getAsString());
-                            }
-                        }
-
-                        if (action.equals("SubjectManager")){
-                            getSubjects();
-                        }
-
-                    } catch (JsonSyntaxException e){
-                        if (received.equals("Heartbeat")) {
-                            lastHeartbeat = System.currentTimeMillis();
-                            System.out.println("Recieved Heartbeat from client " + token + " at " + lastHeartbeat);
-                        } else {
-                            System.out.println("Recieved string: " + received);
-                            writeString(received);
-                        }
-                    }
-                    received = null;
-                }
-            } catch (IOException | SQLException e) {
-                e.printStackTrace();
+            if (action.equals("Account")) {
+              if (jsonObject.get("isRegister").getAsInt() == 1) {
+                createNewUser(jsonObject.get("username").getAsString(), jsonObject.get("hashedpw").getAsString(), jsonObject.get("tutorStatus").getAsInt());
+              } else {
+                  loginUser(jsonObject.get("username").getAsString(), jsonObject.get("hashedpw").getAsString());
+              }
             }
 
+            if (action.equals("SubjectManager")) {
+              getSubjects();
+            }
 
-        }
-
-        System.out.println("Client " + token + " disconnected");
-    }
-
-    public String readString(){
-        try {
-            return dis.readUTF();
-        } catch (IOException e){
-            System.out.println(e);
-            return null;
-        }
-    }
-
-    public void writeString(String msg){
-        try {
-            dos.writeUTF(msg);
-        } catch (IOException IOE){
-            IOE.printStackTrace();
-        }
-    }
-
-    public void loginUser(String username, String password) throws SQLException, IOException {
-        Gson gson = new Gson();
-        if (sqlConnection.checkUserDetails(username, password) == false){
-            JsonElement jsonElement = gson.toJsonTree(AccountLoginResult.FAILED_BY_CREDENTIALS);
-            dos.writeUTF(gson.toJson(jsonElement));
-            System.out.println(gson.toJson(jsonElement));
-        }else{
-            JsonElement jsonElement = gson.toJsonTree(AccountLoginResult.SUCCESS);
-            dos.writeUTF(gson.toJson(jsonElement));
-            System.out.println(gson.toJson(jsonElement));
-        }
-    }
-
-    public void createNewUser(String username, String password, int isTutor) throws IOException {
-        Gson gson = new Gson();
-        if (sqlConnection.getUserDetails(username) == false){
-            if (sqlConnection.createAccount(username, password, isTutor)){
-
-                JsonElement jsonElement = gson.toJsonTree(AccountRegisterResult.SUCCESS);
-                dos.writeUTF(gson.toJson(jsonElement));
+          } catch (JsonSyntaxException e) {
+            if (received.equals("Heartbeat")) {
+              lastHeartbeat = System.currentTimeMillis();
+              System.out.println("Recieved Heartbeat from client " + token + " at " + lastHeartbeat);
             } else {
-                JsonElement jsonElement = gson.toJsonTree(AccountRegisterResult.FAILED_BY_UNEXPECTED_ERROR);
-                dos.writeUTF(gson.toJson(jsonElement));
+              System.out.println("Recieved string: " + received);
+              writeString(received);
             }
-        }else {
-            JsonElement jsonElement = gson.toJsonTree(AccountRegisterResult.FAILED_BY_CREDENTIALS);
-            dos.writeUTF(gson.toJson(jsonElement));
+          }
+          received = null;
         }
+      } catch (IOException | SQLException e) {
+        e.printStackTrace();
+      }
     }
 
-    public void getSubjects(){
+    System.out.println("Client " + token + " disconnected");
+  }
 
+  /**
+   * CLASS DESCRIPTION.
+   * #################
+   *
+   * @author CUBIXEL
+   *
+   */
+  public String readString() {
+    try {
+      return dis.readUTF();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
     }
+}
+  /**
+   * CLASS DESCRIPTION.
+   * #################
+   *
+   * @author CUBIXEL
+   *
+   */
+  public void writeString(String msg) {
+    try {
+      dos.writeUTF(msg);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-    public String toString()
-    {
-        return "This is client " + token;
+  /**
+   * CLASS DESCRIPTION.
+   * #################
+   *
+   * @author CUBIXEL
+   *
+   */
+  public void loginUser(String username, String password) throws SQLException, IOException {
+    Gson gson = new Gson();
+    if (!sqlConnection.checkUserDetails(username, password)) {
+      JsonElement jsonElement = gson.toJsonTree(AccountLoginResult.FAILED_BY_CREDENTIALS);
+      dos.writeUTF(gson.toJson(jsonElement));
+      System.out.println(gson.toJson(jsonElement));
+    } else {
+      JsonElement jsonElement = gson.toJsonTree(AccountLoginResult.SUCCESS);
+      dos.writeUTF(gson.toJson(jsonElement));
+      System.out.println(gson.toJson(jsonElement));
     }
+  }
 
-    public void logOff(){
-        this.loggedIn = false;
+  /**
+   * CLASS DESCRIPTION.
+   * #################
+   *
+   * @author CUBIXEL
+   *
+   */
+  public void createNewUser(String username, String password, int isTutor) throws IOException {
+    Gson gson = new Gson();
+    if (!sqlConnection.getUserDetails(username)) {
+      if (sqlConnection.createAccount(username, password, isTutor)) {
+        JsonElement jsonElement = gson.toJsonTree(AccountRegisterResult.SUCCESS);
+        dos.writeUTF(gson.toJson(jsonElement));
+      } else {
+        JsonElement jsonElement = gson.toJsonTree(AccountRegisterResult.FAILED_BY_UNEXPECTED_ERROR);
+        dos.writeUTF(gson.toJson(jsonElement));
+      }
+    } else {
+      JsonElement jsonElement = gson.toJsonTree(AccountRegisterResult.FAILED_BY_CREDENTIALS);
+      dos.writeUTF(gson.toJson(jsonElement));
     }
+  }
+
+  public void getSubjects(){
+
+  }
+
+  public String toString() {
+    return "This is client " + token;
+  }
+
+  public void logOff() {
+    this.loggedIn = false;
+  }
 }
