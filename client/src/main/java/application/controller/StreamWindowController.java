39fb17d3485c615886971ba329fe193474bcead8
James Gardner <iamjamesgardner@googlemail.com>
2020-03-16 07:55:41 +0000

    Updated StreamWindowController

--- client/src/main/java/application/controller/StreamWindowController.java
+++ client/src/main/java/application/controller/StreamWindowController.java
@@ -1,5 +1,32 @@
 package application.controller;
 
-public class StreamWindowController {
+import application.controller.services.MainConnection;
+import application.view.ViewFactory;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TabPane;
 
+public class StreamWindowController extends BaseController implements Initializable {
+  
+  @FXML
+  private TabPane primaryTabPane;
+
+  /**
+   * CONSTRUCTOR DESCRIPTION.
+   *
+   * @param viewFactory
+   * @param fxmlName
+   * @param mainConnection
+   */
+  public StreamWindowController(ViewFactory viewFactory, String fxmlName,
+      MainConnection mainConnection) {
+    super(viewFactory, fxmlName, mainConnection);
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+
+  }
 }
