947c7826ddb68c50aa6b6df834101a466db76c7b
James Gardner <iamjamesgardner@googlemail.com>
2020-04-05 09:22:24 +0100

    Javadocs

--- server/src/main/java/services/ServerTools.java
+++ server/src/main/java/services/ServerTools.java
@@ -16,7 +16,13 @@ import org.slf4j.LoggerFactory;
 import services.enums.SubjectRequestResult;
 import sql.MySql;
 
-public class ServerTools {
+/**
+ * ServerTools contains a set of Static methods used
+ * for packaging and sending data.
+ *
+ * @author James Gardner
+ */
+public final class ServerTools {
 
   private static final Logger log = LoggerFactory.getLogger("Server Logger");
 
@@ -42,10 +48,26 @@ public class ServerTools {
     dis.close();
   }
 
+
   /**
-   * Based on the number of previous requests gets the next subject from the
-   * database and creates a new Subject class which is then packaged as a json
-   * and written to the DataOutputStream. numberOfSubjectsSent is then incremented.
+   * Based on the number of subjects already sent this method gets the next five
+   * subject from the database and creates a new Subject class which is then
+   * packaged as a json and written to the DataOutputStream. If five more
+   * subjects are not available it sends as many as it can and then breaks out
+   * the loop. Each Subject is preceded by a String with the state of that
+   * request.
+   *
+   * @param dos
+   *        The DataOutputStream to write the Subjects too
+   *
+   * @param sqlConnection
+   *        The Class that connects to the MySQL Database
+   *
+   * @param numberOfSubjectsSent
+   *        The number of subjects already sent to the Client
+   *
+   * @throws SQLException
+   *         If failure to access MySQL database.
    */
   public static void getSubjectService(DataOutputStream dos, MySql sqlConnection,
       int numberOfSubjectsSent) throws SQLException {
@@ -91,7 +113,7 @@ public class ServerTools {
         }
       }
     } catch (IOException e) {
-      e.printStackTrace();
+      log.error("ServerTools: getSubjectService, error writing to DataOutputStream ", e);
     }
   }
 
--- server/src/main/java/services/enums/SubjectRequestResult.java
+++ server/src/main/java/services/enums/SubjectRequestResult.java
@@ -3,5 +3,6 @@ package services.enums;
 public enum SubjectRequestResult {
   SUCCESS,
   FAILED_BY_NETWORK,
-  FAILED_BY_NO_MORE_SUBJECTS
+  FAILED_BY_NO_MORE_SUBJECTS,
+  FAILED_BY_DATABASE_ACCESS
 }
