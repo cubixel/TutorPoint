0b56b8476fe5e338034e86afd95940d30d3ba338
DasGabelhauser <ew1150@york.ac.uk>
2020-05-13 15:17:24 +0100

    Tested javadocced all presentation.exceptions

--- client/src/main/java/application/controller/presentation/exceptions/DocumentInfoException.java
+++ client/src/main/java/application/controller/presentation/exceptions/DocumentInfoException.java
@@ -1,10 +1,20 @@
 package application.controller.presentation.exceptions;
 
-
+/**
+ * This in an exception thrown when a problem is found with
+ * a presentation documentinfo Node.
+ * @author CUBIXEL
+ * @see application.controller.presentation.PresentationObject
+ */
 public class DocumentInfoException extends Exception {
 
   private static final long serialVersionUID = 1L;
 
+  /**
+   * Constructor for DocumentInfoException.
+   * @param errorMessage A message explaining why the exception was thrown.
+   * @param err A previously thrown exception or a new Throwable.
+   */
   public DocumentInfoException(String errorMessage, Throwable err) {
     super(errorMessage, err);
   }
--- client/src/main/java/application/controller/presentation/exceptions/XmlLoadingException.java
+++ client/src/main/java/application/controller/presentation/exceptions/XmlLoadingException.java
@@ -1,10 +1,20 @@
 package application.controller.presentation.exceptions;
 
-
+/**
+ * This in an exception thrown when a problem is found while
+ * loading a presentation XML.
+ * @author CUBIXEL
+ * @see application.controller.presentation.XmlHandler
+ */
 public class XmlLoadingException extends Exception {
 
   private static final long serialVersionUID = 1L;
 
+  /**
+   * Constructor for XmlLoadingException.
+   * @param errorMessage A message explaining why the exception was thrown.
+   * @param err A previously thrown exception or a new Throwable.
+   */
   public XmlLoadingException(String errorMessage, Throwable err) {
     super(errorMessage, err);
   }
