702f388bebf3ba9b10e971919636a287d6ec4f3e
James Gardner <iamjamesgardner@googlemail.com>
2020-05-14 10:38:53 +0100

    Javadocs for Requests

--- client/src/main/java/application/model/requests/FollowSubjectRequest.java
+++ client/src/main/java/application/model/requests/FollowSubjectRequest.java
@@ -1,9 +1,27 @@
 package application.model.requests;
 
+/**
+ * This Class is used to contain the information used for
+ * following a subject. It is packaged as a Json and sent to
+ * the server.
+ *
+ * @author James Gardner
+ * @see application.controller.services.FollowSubjectRequestService
+ * @see application.controller.SubjectWindowContoller
+ */
 public class FollowSubjectRequest {
   int subjectID;
   boolean isFollowing;
 
+  /**
+   * Constructor for the FollowSubjectRequest.
+   *
+   * @param subjectID
+   *        A unique subject ID that is assigned by the MySql Database
+   *
+   * @param isFollowing
+   *        Is the User is currently following the subject or not
+   */
   public FollowSubjectRequest(int subjectID, boolean isFollowing) {
     this.subjectID = subjectID;
     this.isFollowing = isFollowing;
--- client/src/main/java/application/model/requests/FollowTutorRequest.java
+++ client/src/main/java/application/model/requests/FollowTutorRequest.java
@@ -1,9 +1,27 @@
 package application.model.requests;
 
+/**
+ * This Class is used to contain the information used for
+ * following a tutor. It is packaged as a Json and sent to
+ * the server.
+ *
+ * @author James Gardner
+ * @see application.controller.services.FollowTutorRequestService
+ * @see application.controller.TutorWindowContoller
+ */
 public class FollowTutorRequest {
   int tutorID;
   boolean isFollowing;
 
+  /**
+   * Constructor for the FollowTutorRequest.
+   *
+   * @param tutorID
+   *        A userID of the tutor that is assigned to a user upon account creation
+   *
+   * @param isFollowing
+   *        Is the User is currently following the subject or not
+   */
   public FollowTutorRequest(int tutorID, boolean isFollowing) {
     this.tutorID = tutorID;
     this.isFollowing = isFollowing;
