8d9b3c5515411d8e834c7537b574a693121ba9d6
dmb537 <dmb537@york.ac.uk>
2020-05-30 14:20:25 +0100

    Fix restarting webcam

--- client/src/main/java/application/controller/services/WebcamService.java
+++ client/src/main/java/application/controller/services/WebcamService.java
@@ -127,63 +127,63 @@ public class WebcamService extends Thread {
     // Mixer mixer = AudioSystem.getMixer(minfoSet[AUDIO_DEVICE_INDEX]);
     DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
 
+    final TargetDataLine line = getAudioLine(dataLineInfo);
     ScheduledThreadPoolExecutor audioCapture = null;
-    try {
-      // Open and start capturing audio
-      // It's possible to have more control over the chosen audio device with this line:
-      // TargetDataLine line = (TargetDataLine)mixer.getLine(dataLineInfo);
-      final TargetDataLine line = (TargetDataLine)AudioSystem.getLine(dataLineInfo);
-      line.open(audioFormat);
-      line.start();
+    if (line != null) {
+      try {
+        // Open and start capturing audio
+        line.open(audioFormat);
+        line.start();
 
-      final int sampleRate = (int) audioFormat.getSampleRate();
-      final int numChannels = audioFormat.getChannels();
+        final int sampleRate = (int) audioFormat.getSampleRate();
+        final int numChannels = audioFormat.getChannels();
 
-      // Let's initialize our audio buffer...
-      final int audioBufferSize = sampleRate * numChannels;
-      final byte[] audioBytes = new byte[audioBufferSize];
+        // Let's initialize our audio buffer...
+        final int audioBufferSize = sampleRate * numChannels;
+        final byte[] audioBytes = new byte[audioBufferSize];
 
-      // Using a ScheduledThreadPoolExecutor vs a while loop with
-      // a Thread.sleep will allow
-      // us to get around some OS specific timing issues, and keep
-      // to a more precise
-      // clock as the fixed rate accounts for garbage collection
-      // time, etc
-      // a similar approach could be used for the webcam capture
-      // as well, if you wish
-      audioCapture = new ScheduledThreadPoolExecutor(1);
-      audioCapture.scheduleAtFixedRate(new Runnable() {
-        @Override
-        public void run() {
-          try {
-            // Read from the line... non-blocking
-            int nbytesRead = 0;
-            while (nbytesRead == 0) {
-              nbytesRead = line.read(audioBytes, 0, line.available());
+        // Using a ScheduledThreadPoolExecutor vs a while loop with
+        // a Thread.sleep will allow
+        // us to get around some OS specific timing issues, and keep
+        // to a more precise
+        // clock as the fixed rate accounts for garbage collection
+        // time, etc
+        // a similar approach could be used for the webcam capture
+        // as well, if you wish
+        audioCapture = new ScheduledThreadPoolExecutor(1);
+        audioCapture.scheduleAtFixedRate(new Runnable() {
+          @Override
+          public void run() {
+            try {
+              // Read from the line... non-blocking
+              int nbytesRead = 0;
+              while (nbytesRead == 0) {
+                nbytesRead = line.read(audioBytes, 0, line.available());
+              }
+
+              // Since we specified 16 bits in the AudioFormat,
+              // we need to convert our read byte[] to short[]
+              // (see source from FFmpegFrameRecorder.recordSamples for AV_SAMPLE_FMT_S16)
+              // Let's initialize our short[] array
+              int nsamplesRead = nbytesRead / 2;
+              short[] samples = new short[nsamplesRead];
+
+              // Let's wrap our short[] into a ShortBuffer and
+              // pass it to recordSamples
+              ByteBuffer.wrap(audioBytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(samples);
+              ShortBuffer shortBuff = ShortBuffer.wrap(samples, 0, nsamplesRead);
+
+              // recorder is instance of
+              // org.bytedeco.javacv.FFmpegFrameRecorder
+              recorder.recordSamples(sampleRate, numChannels, shortBuff);
+            } catch (org.bytedeco.javacv.FrameRecorder.Exception e) {
+              e.printStackTrace();
             }
-
-            // Since we specified 16 bits in the AudioFormat,
-            // we need to convert our read byte[] to short[]
-            // (see source from FFmpegFrameRecorder.recordSamples for AV_SAMPLE_FMT_S16)
-            // Let's initialize our short[] array
-            int nsamplesRead = nbytesRead / 2;
-            short[] samples = new short[nsamplesRead];
-
-            // Let's wrap our short[] into a ShortBuffer and
-            // pass it to recordSamples
-            ByteBuffer.wrap(audioBytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(samples);
-            ShortBuffer shortBuff = ShortBuffer.wrap(samples, 0, nsamplesRead);
-
-            // recorder is instance of
-            // org.bytedeco.javacv.FFmpegFrameRecorder
-            recorder.recordSamples(sampleRate, numChannels, shortBuff);
-          } catch (org.bytedeco.javacv.FrameRecorder.Exception e) {
-            e.printStackTrace();
           }
-        }
-      }, 0, (long) 1000 / FRAME_RATE, TimeUnit.MILLISECONDS);
-    } catch (LineUnavailableException e1) {
-      e1.printStackTrace();
+        }, 0, (long) 1000 / FRAME_RATE, TimeUnit.MILLISECONDS);
+      } catch (LineUnavailableException e1) {
+        e1.printStackTrace();
+      }
     }
 
     Frame capturedFrame = null;
@@ -244,13 +244,27 @@ public class WebcamService extends Thread {
         audioCapture.shutdown();
         audioCapture.awaitTermination(500, TimeUnit.MILLISECONDS);
       }
-      recorder.close();
+      if (line != null) {
+        line.close();
+      }
       grabber.close();
+      recorder.close();
     } catch (FrameRecorder.Exception | Exception | InterruptedException e) {
       e.printStackTrace();
     }
 
 
+  }
+
+  private TargetDataLine getAudioLine(DataLine.Info dataLineInfo) {
+    // It's possible to have more control over the chosen audio device with this line:
+    // TargetDataLine line = (TargetDataLine)mixer.getLine(dataLineInfo);
+    try {
+      return (TargetDataLine) AudioSystem.getLine(dataLineInfo);
+    } catch (LineUnavailableException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 
   public void killService() {
